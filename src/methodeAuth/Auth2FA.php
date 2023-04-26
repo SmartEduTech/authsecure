13ddd34f6211071028485fdc96e14d1c41db8234
amalkorbi96 <amalkorbi96@gmail.com>
2023-04-26 19:04:25 +0100

    Update Auth2FA.php

--- src/methodeAuth/Auth2FA.php
+++ src/methodeAuth/Auth2FA.php
@@ -6,26 +6,41 @@ require_once 'vendor/autoload.php';
 function Envoyer_code_secret($email) {
 
     // Génère un code aléatoire à cinq chiffres
-    $code = rand(10000, 99999);
+    $code_secret = rand(10000, 99999);
     
     // Configure les paramètres du serveur SMTP pour l'envoi d'e-mails
-    $transport = (new Swift_SmtpTransport('smtp.example.com', 587, 'tls'))
-        ->setUsername('your-email@example.com')
-        ->setPassword('your-email-password');
+    $transport = (new Swift_SmtpTransport('UVT.com', 587, 'tls'))
+        ->setUsername('UVT.com')
+        ->setPassword('UVT-password');
 
     // Crée l'instance du mailer SwiftMailer en utilisant le transport SMTP configuré
     $mailer = new Swift_Mailer($transport);
 
     // Crée le message e-mail
     $message = (new Swift_Message('Code de vérification à deux facteurs'))
-        ->setFrom(['your-email@example.com' => 'Your Name'])
+        ->setFrom(['your-email@example.com' => 'UVT'])
         ->setTo([$email])
-        ->setBody("Votre code de vérification à deux facteurs est : $code");
+        ->setBody("Votre code de vérification à deux facteurs est : $code_secret");
 
     // Envoie le message e-mail
     $result = $mailer->send($message);
 
     // Retourne le code de vérification généré
-    return $code;
+    return $code_secret;
 }
+function verifier_code_secret($email,$code_utilisateur) {
+// Envoyer un code de vérification à deux facteurs à l'utilisateur
+$code_secret = Envoyer_code_secret($email);
+
+// Demander à l'utilisateur de saisir le code de vérification
+$code_utilisateur = $_POST['code'];
+
+// Vérifier si le code de vérification correspond à celui envoyé par e-mail
+if ($code_utilisateur == $code_secret) {
+    echo 'Code de vérification valide.';
+} else {
+    echo 'Code de vérification invalide.';
+}
+}
+
 ?>
