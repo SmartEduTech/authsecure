4ddd1e0e8894368a1ef5aa655843d6e0bd45d8a6
amalkorbi96 <amalkorbi96@gmail.com>
2023-04-27 20:49:41 +0100

    Update Auth2FA.php

--- src/methodeAuth/Auth2FA.php
+++ src/methodeAuth/Auth2FA.php
@@ -1,46 +1,56 @@
 <?php 
-
+// Inclure SwiftMailer
 require_once 'vendor/autoload.php'; 
 
- $email = $_POST['email'];
-function Envoyer_code_secret($email) {
+class auth2FA extends Authentification {
+  
+  // Constructeur
+  public function __construct($id_authentification, Utilisateur $id_utilisateur, $date_connexion, $adresse_ip) {
+    parent::__construct($id_authentification, $id_utilisateur, $date_connexion, $adresse_ip);
+  }
 
-    // Génère un code aléatoire à cinq chiffres
-    $code_secret = rand(10000, 99999);
-    
+  // Fonction pour envoyer le code secret par email
+  public function envoyer_code_secret($email) {
+    // Générer le code secret
+    $code_secret = rand(100000, 999999);
+
     // Configure les paramètres du serveur SMTP pour l'envoi d'e-mails
-    $transport = (new Swift_SmtpTransport('UVT.com', 587, 'tls'))
-        ->setUsername('UVT.com')
+    $transport = (new Swift_SmtpTransport('UVT.com', 123, 'tls'))
+        ->setUsername('UVT@example.com')
         ->setPassword('UVT-password');
 
-    // Crée l'instance du mailer SwiftMailer en utilisant le transport SMTP configuré
+    // Crée l'objet SwiftMailer avec le transport SMTP configuré
     $mailer = new Swift_Mailer($transport);
 
-    // Crée le message e-mail
-    $message = (new Swift_Message('Code de vérification à deux facteurs'))
-        ->setFrom(['your-email@example.com' => 'UVT'])
+    // Crée l'objet message
+    $message = (new Swift_Message('Code secret pour la vérification à deux facteurs'))
+        ->setFrom(['UVT@example.com' => 'UVT'])
         ->setTo([$email])
-        ->setBody("Votre code de vérification à deux facteurs est : $code_secret");
+        ->setBody("Votre code secret pour la vérification à deux facteurs est : " . $code_secret);
 
-    // Envoie le message e-mail
+    // Envoie le message
     $result = $mailer->send($message);
 
-    // Retourne le code de vérification généré
+    // Retourne le code secret généré
     return $code_secret;
-}
-function verifier_code_secret($email,$code_utilisateur) {
-// Envoyer un code de vérification à deux facteurs à l'utilisateur
-$code_secret = Envoyer_code_secret($email);
+  }
 
-// Demander à l'utilisateur de saisir le code de vérification
-$code_utilisateur = $_POST['code'];
+  // Fonction pour vérifier le code secret
+  public function verifier_code_secret($code_secret) {
+    // Récupérer le code secret stocké dans la session
+    $code_secret_stocke = $_SESSION['code_secret'];
 
-// Vérifier si le code de vérification correspond à celui envoyé par e-mail
-if ($code_utilisateur == $code_secret) {
-    echo 'Code de vérification valide.';
-} else {
-    echo 'Code de vérification invalide.';
+    // Vérifier si les codes secrets correspondent
+    if ($code_secret == $code_secret_stocke) {
+      // Si les codes correspondent, supprimer le code secret stocké dans la session et retourner vrai
+      unset($_SESSION['code_secret']);
+      return true;
+    } else {
+      // Si les codes ne correspondent pas, retourner faux
+      return false;
+    }
+  }
 }
-}
+
 
 ?>
